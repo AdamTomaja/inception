74c0326a833bfd8baa233f6dfcc579d786d88af7
adamtomaja <adiscool@tlen.pl>
2017-04-18 23:09:01 +0200

    Look in max distance

--- src/main/java/com/cydercode/inception/controller/action/LookAction.java
+++ src/main/java/com/cydercode/inception/controller/action/LookAction.java
@@ -4,13 +4,15 @@ import com.cydercode.inception.controller.Command;
 import com.cydercode.inception.events.server.ConsoleEvent;
 import com.cydercode.inception.game.Game;
 import com.cydercode.inception.game.TreeTraverser;
+import com.cydercode.inception.model.Matter;
 import com.cydercode.inception.model.Node;
 import com.cydercode.inception.model.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 @Component
 @ActionName("look")
@@ -23,10 +25,16 @@ public class LookAction implements PlayerAction {
     public void execute(Player player, Game game, Command command) {
         Node parentWorld = treeTraverser.findParent(player, game).get();
 
-        List<Node> result = new ArrayList<>(parentWorld.getChildren());
-        result.remove(player);
+        Stream<Node> nodeStream = parentWorld.getChildren().stream().filter(n -> n != player);
+
+        if (!command.getParameters().isEmpty()) {
+            double distance = Double.parseDouble(command.getParameters().get(0));
+            nodeStream = nodeStream.filter(n -> n instanceof Matter)
+                    .filter(n -> ((Matter) n).getLocation().distanceFrom(player.getLocation()) < distance);
+        }
+
         StringBuilder stringBuilder = new StringBuilder();
-        for (Node node : result) {
+        for (Node node : nodeStream.collect(toList())) {
             stringBuilder.append(node.toString());
             stringBuilder.append(", ");
         }
